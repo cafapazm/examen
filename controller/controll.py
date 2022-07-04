89138c519f3f331bf9cdba675755192c91f9eb30
Fabian <cafapazm@espol.edu.ec>
2022-07-04 12:59:47 -0500

    Examen

--- controller/controll.py
+++ controller/controll.py
@@ -1,3 +1,12 @@
 import streamlit as st
+from PIL import Image
+icon=Image("imagen")
+st.title("**Ingeniería de Producción**")
 
-st.sidebar.title("**Ingeniería de Producción**")
+st.write("Esta aplicación tiene el objetivo de presentar"
+         "información relacionada a los caudales"
+         "de producción")
+st.sidebar.title(":around_down_small:*Production* ")
+uploaded_file= st.sidebar.file_uploader("Upload your csv file here")
+st.sidebar.radio("pages", options=["Home", "Data", "Basic Calculations"])
+streamlit run controll.py
